39c021393c86381d4b7deffee6763cb6a211a493
Abdulrahman1789 <fouadabdulrahman176@gmail.com>
2026-06-24 13:44:53 +0300

    feat: add user model and register auth service

---
+++ src/models/user.model.ts
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            enum: ["employee", "hr", "admin"],
+            default: "employee",
+        },
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+
+    },
+    { timestamps: true }
+);
+
+export const User = model("User", userSchema);
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,10 +1,28 @@
 import { RegisterInput } from "./../modules/auth/auth.types";
+import bycrpt from "bcrypt";
+import { User } from "../models/user.model";
+
+const register = async (payload: any) => {
+    const isUserExists = await User.findOne({
+        email: payload.email,
+    });
+
+    if (isUserExists) {
+        throw new Error("User already exists");
+    }
+
+    const hashedPassword = await bycrpt.hash(
+        payload.password,
+        10
+    );
+
+    payload.password = hashedPassword;
+
+    const result = await User.create(payload);
+
+    return result;
+};
 
 export const AuthService = {
-    register: async (data: RegisterInput) => {
-        return {
-            message: "service working",
-            data,
-        };
-    }
+    register,
 }
