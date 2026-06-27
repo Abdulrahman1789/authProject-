3b37bb6e0d7c4aeec6d17552c15bb3ce17f90b00
Abdulrahman1789 <fouadabdulrahman176@gmail.com>
2026-06-27 14:46:56 +0300

    feat: add database configuration and connection utility using mongoose

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
+import { env } from "./env";
 
 export const connectDB = async (): Promise<void> => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI! as string)
+        if (!env.MONGO_URI) {
+            throw new Error("MONGO_URI environment variable is not defined.");
+        }
+        await mongoose.connect(env.MONGO_URI);
         console.log("Connected to DB");
     } catch (error) {
-        console.error("Database Connection Failed", error);
+        console.error("Database Connection Failed:", error);
         process.exit(1);
     }
-}
+};
